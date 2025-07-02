a4f44b3b0358bd01090b829b9f0cdf8caece0567
sina <sina.sipamo@gmail.com>
2025-07-02 15:24:33 +0330

    Work with Django admin

--- 3/job_board/models.py
+++ 3/job_board/models.py
@@ -12,6 +12,9 @@ class JobPosting(models.Model):
     salary = models.IntegerField()
     is_active = models.BooleanField(default=False)
 
+    def __str__(self):
+        return f"{self.pk}: {self.title} | {self.company} | Active: {self.is_active}"  
+
 # makemigrations
 ## -> creates instructions telling django how the db have changed
 # migrate
